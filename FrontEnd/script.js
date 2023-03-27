26946788e4fc0bf6830ba9195d5266be5f554a7a
CodrieN <adrien.marguin@gmail.com>
2023-03-27 21:32:38 +0200

    .

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,59 +1,29 @@
-// todo récuperer works
+const gallery = document.getElementById("gallery");
 
-// ? déclarer Fetch
-
-const fetchResponsePromise = Promise<Response> fetch(entrée[, init]);
-
-
+const newImg = document.createElement("img")
+newImg.innerHTML = "http://localhost:5678/api/works"
+document.querySelector("#gallery").append(newImg)
 
 
-if (window.fetch) {
-}
-console.log(fetch);
-// else {
-//     // Faire quelque chose avec XMLHttpRequest?
+// *apend pourra être remplacé par prepend selon que l'on souhaitera faire apparaitre 
+// *les nouveaux éléments depuis le début ou la fin de la liste.
+// *il est aussi possible d'utiliser insertAdjacentElement("afterbefin)") <===exemple
+
+
+console.log(gallery);
+
+fetch("http://localhost:5678/api/works")
+.then(r => r.json())
+.then(body => console.log(body))
+
+
+
+
+
+
+
+// async function fetchData (){
+// const r = await fetch("http://localhost:5678/api/works")
 // }
 
-// **https://developer.mozilla.org/fr/docs/Web/API/Request
-
-// !Dans l'extrait de code suivant, nous créons une nouvelle
-// !requête à l'aide du constructeur Request() avec des
-//  !données initiales et du contenu du body pour une requête
-//   !api qui nécessite une charge utile de body:
-
-// *const request = new Request('http://localhost:5678/api/works', {method: 'POST', body: '{"foo": "bar"}'});
-
-// *const URL = request.url;
-// *const method = request.method;
-// *const credentials = request.credentials;
-// *const bodyUsed = request.bodyUsed;
-
-// !Vous pouvez ensuite récupérer cette demande d'API
-// !en passant l'objet Request en tant que paramètre à
-//  !un appel fetch(), par exemple et obtenir la réponse:
-
-// *fetch(request)
-//   *.then(response => {
-//     *if (response.status === 200) {
-//       *return response.json();
-//     *} else {
-//       *throw new Error('Something went wrong on api server!');
-//     *}
-//   *})
-//   *.then(response => {
-//     *console.debug(response);
-//     // ...
-//   *}).catch(error => {
-//     *console.error(error);
-//   *});
-
-// **https://developer.mozilla.org/fr/docs/Web/API/Document/querySelector
-
-// todo récupérer les travaux depuis l'API
-// ** https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
-
-// todo ajouter les button filtres en JS
-
-// const buttonfilter = document.querySelector (".gallery");
-
-// console.log (buttonfilter);
+const loader = document.createElement("img") 
