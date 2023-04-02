5b09f2cb9989009daf621c9508051c63fbf9ac93
CodrieN <adrien.marguin@gmail.com>
2023-04-02 11:53:52 +0200

    utilisation de .set sans succes

--- FrontEnd/fonction.js
+++ FrontEnd/fonction.js
@@ -1,21 +1,34 @@
-  fetch('http://localhost:5678/api/works')
-    .then(response => response.json())
-    .then(data => {
+fetch("http://localhost:5678/api/works")
+  .then((response) => response.json())
+  .then((data) => {
+    //* ---------------gallery-------------------------------------------------
 
-      data.forEach(image => {
-        const imageGrid = document.querySelector('.gallery');
-        const figureElement = document.createElement('figure');
-        const imageElement = document.createElement('img');
-        const captionElement = document.createElement('figcaption');
+    data.forEach((works) => {
+      const imageGrid = document.querySelector(".gallery");
+      const figureElement = document.createElement("figure");
+      const imageElement = document.createElement("img");
+      const captionElement = document.createElement("figcaption");
 
-        imageElement.src = image.imageUrl;
-        captionElement.textContent = image.title;
+      imageElement.src = works.imageUrl;
+      captionElement.textContent = works.title;
 
-        figureElement.appendChild(imageElement);
-        figureElement.appendChild(captionElement);
-        imageGrid.appendChild(figureElement);
-      });
-    })
-    .catch(error => console.error(error));
+      figureElement.appendChild(imageElement);
+      figureElement.appendChild(captionElement);
+      imageGrid.appendChild(figureElement);
+    });
 
+    //* ---------------buttons-------------------------------------------------
+
+    // * ajout des boutons
+
+    data.forEach((works) => {
+      const buttonsFilter = document.querySelector(".buttons");
+      const setCategoryEllement = document.createElement("button");
+ 
+    setCategoryEllement.classList.add("btn");
+    setCategoryEllement.textContent = works.category.name;
+    buttonsFilter.appendChild(setCategoryEllement);
     
+  });
+  
+  });
