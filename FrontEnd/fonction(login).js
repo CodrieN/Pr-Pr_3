36c5335e73f204646eac872783bae6930cc6364f
CodrieN <adrien.marguin@gmail.com>
2023-04-12 19:36:21 +0200

    post login en fetch actif, avec ajout de ' if (data.userId && data.token) '

--- FrontEnd/fonction(login).js
+++ FrontEnd/fonction(login).js
@@ -1,14 +1,11 @@
 // ! Verification Login*****************************************************************************************
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+  e.preventDefault(); 
 
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   verifyLogin(email, password);
-
-  console.log(email);
-  console.log(password);
 });
 
 function verifyLogin(email, password) {
@@ -22,7 +19,8 @@ function verifyLogin(email, password) {
     .then((response) => response.json())
     .then((data) => {
       // Vérifier la réponse du serveur
-      if (data.success) {
+      // !console.log(data);
+      if (data.userId && data.token) {
         // Connexion réussie, rediriger l'utilisateur vers la page d'accueil
         window.location.href = "./index.html";
       } else {
@@ -38,4 +36,11 @@ function verifyLogin(email, password) {
 
 
 // ? https://github.com/CodrieN/Pr-Pr_3/blob/secondary/Backend/controllers/users.controller.js
+
+// todo | penser à utiliser des .innerText =  "logout" et .innerText = "login"  dans un if/else au moment
+// todo | où l'on vérifie si le token est "stocké", innerText permet en js de changer directement le texte 
+// todo | d'un élément HTML
+
+localStorage.setItem('token', data.token);
+localStorage.setItem('tokenExpire', new Date().getTime() + 60 * 60 * 1000); // faire expirer au bout de 60 minutes
 
