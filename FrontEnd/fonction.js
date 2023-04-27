5f047e5afe48e90f79e3ffd78e99b056df1d7992
CodrieN <adrien.marguin@gmail.com>
2023-04-27 21:42:32 +0200

    ajout de fetch delete | travail sur modal 1/3 avec remplacement de croix  (manque encore la fermetude close() hors modal | ajout formulaire modal 2/3 |

--- FrontEnd/fonction.js
+++ FrontEnd/fonction.js
@@ -1,9 +1,8 @@
-// ! Gallery*************************************************************************
-
 const buttonsFilter = document.querySelector(".buttons");
 const buttonsCategories = new Set();
 const gallery = document.querySelector(".gallery");
 const modalWrapper = document.querySelector(".modal-wrapper");
+const modalGrid = document.querySelector("#modalGrid");
 
 function creatButton(content) {
   const buttonElement = document.createElement("button");
@@ -11,15 +10,55 @@ function creatButton(content) {
   buttonsFilter.appendChild(buttonElement);
   buttonElement.classList.add("btn");
 }
-function createWork(work, container) {
+function createWork(work, container, type = "gallery") {
   const figureElement = document.createElement("figure");
   const imageElement = document.createElement("img");
   const captionElement = document.createElement("figcaption");
 
   imageElement.src = work.imageUrl;
-  captionElement.textContent = work.title;
 
   figureElement.appendChild(imageElement);
+  if (type === "gallery") {
+    captionElement.textContent = work.title;
+  }
+  if (type === "modal") {
+    captionElement.textContent = "éditer";
+    const trash = document.createElement("i");
+    const cross = document.createElement("i");
+    trash.classList.add("fa-solid", "fa-trash-can");
+    cross.classList.add("fa-solid", "fa-arrows-up-down-left-right");
+
+    figureElement.appendChild(cross);
+    figureElement.appendChild(trash);
+
+    imageElement.addEventListener("mouseover", (event) => {
+      event.target.nextElementSibling.style.display = "block";
+      console.log("ca fonctionne");
+    });
+
+    imageElement.addEventListener("mouseout", (event) => {
+      event.target.nextElementSibling.style.display = "none";
+    });
+
+    cross.style.display = "none";
+
+    cross.addEventListener("mouseover", (event) => {
+      event.target.style.display = "block";
+    });
+
+    cross.addEventListener("mouseout", (event) => {
+      event.target.style.display = "block";
+    });
+
+    trash.addEventListener("click", () => {
+      fetch("http://localhost:5678/api/works/" + work.id, {
+        method: "DELETE",
+        headers: { Authorization: "Bearer " + localStorage.getItem("token") },
+      }).then((response) => {
+        console.log(response);
+      });
+    });
+  }
   figureElement.appendChild(captionElement);
   container.appendChild(figureElement);
 }
@@ -31,7 +70,7 @@ fetch("http://localhost:5678/api/works")
 
     data.forEach((work) => {
       createWork(work, gallery);
-      createWork(work, modalGrid);
+      createWork(work, modalGrid, "modal");
 
       // ***************boutons filtre*******************************************
       buttonsCategories.add(work.category.name);
@@ -56,7 +95,7 @@ fetch("http://localhost:5678/api/works")
           // on recré une nouvelle figure pour chaque works
 
           data.forEach((work) => {
-            createWork(work);
+            createWork(work, gallery);
           });
         } else {
           // on filtre le tableau des works par category
@@ -66,7 +105,7 @@ fetch("http://localhost:5678/api/works")
 
           // création des figures selon la category selectionnée
           filteredWorks.forEach((work) => {
-            createWork(work);
+            createWork(work, gallery);
           });
         }
       });
@@ -103,16 +142,11 @@ if (token !== null) {
 
 //***************MODAL*******************************************
 
-// todo if création works dans modale, condition => afficher les icones et texte éditer
-
 const BtnModificationWorks = document.querySelector("#adminWorks");
 
 BtnModificationWorks.addEventListener("click", () => {
   // console.log(BtnModificationWorks);
-  modalWrapper.showModal(); // ? toujours capable de scoll le <main> ?
-
-
-function openCheck(modalWrapper) {
+  modalWrapper.showModal();
   if (modalWrapper.open) {
     console.log("ca marche");
     modalGrid.style.display = "grid";
@@ -120,34 +154,74 @@ function openCheck(modalWrapper) {
     modalGrid.style.gridGap = "10px 10px";
     modalGrid.style.gridTemplateColumns = "auto auto auto auto auto";
     modalGrid.style.gridTemplateRow = "300px 300px 300px ";
-
-  }else{
+  } else {
     modalWrapper.style.display = "none";
-    }
-
-}
-openCheck(modalWrapper);
-
-// ! boucle ?  while(true) ?
-// while (modalWrapper.open === true) {
-//       console.log("ca marche");
-//       modalWrapper.style.display = "grid";
-//       modalWrapper.style.gridGap = "10px 10px";
-//       modalWrapper.style.gridTemplateColumns = "(5, 1fr)";
-// }
-// ! boucle ?
-});
-
-
-
-const toCloseDialog = document.getElementById('toCloseDialog');
-
-toCloseDialog.addEventListener('click', (event) => {
-  if (event.target.id !== 'toCloseDialog') {
-      modalWrapper.close();
   }
 });
 
+// * fermer la modale au click sur la croix || hors modal----------------------------------
+
+const x = document.querySelector(".fa-xmark");
+
+x.addEventListener("click",
+  () => {
+    console.log("cela fonctionne bien");
+    modalWrapper.close();
+  });
 
 
 
+// * fermer la modale hors champ -----------------------------------------------------------------------------------
+
+// modalWrapper.addEventListener('click', (e) => {
+//   if (e.target.id !== "modal-wrapper") {
+//     console.log("fermer la modal");
+//       modalWrapper.close();
+//       e.stopPropagation();
+//   }
+// });
+
+// * au click sur btnAddPic => modal 2/3----------------------------------------------------------------------------
+
+// todo ajouter toggle dans une fonction
+
+const btnAddPic = document.querySelector(".btnAddPic");
+const modalFooter = document.querySelector("#modalFooter");
+const titleModal = document.querySelector("#titleModal");
+
+const arrow = document.querySelector("#arrow");
+btnAddPic.addEventListener("click", () => {
+  if (modalWrapper.open) {
+    btnAddPic.remove();
+    modalFooter.remove();
+    titleModal.textContent = "Ajout photo";
+    modalGrid.remove();
+
+    // ajout de fleche gauche
+    arrow.style.display = "flex";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+  }
+});
+
+//  todo ajout icon image modal 2/3 <i class="fa-regular fa-image"></i>
+
+// todo formData
+    // http://localhost:5678/api/works
